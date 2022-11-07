4605e1224a8557cf1bdf2fc073966ec915657569
Hoang <dsklvietsub@gmail.com>
2022-11-07 10:38:39 +0700

    hahaha

--- random_check.py
+++ random_check.py
@@ -6,16 +6,20 @@ import random
 import shutil
 
 def copy_file(sv, src_path, output_dir, mark):
+    txt_src_path = os.path.splitext(src_path)[0] + '.txt'
     src_name = os.path.basename(src_path)
+    txt_src_name = os.path.basename(txt_src_path)
     dest_dir = f'{output_dir}/{mark}/{sv}/'
     os.makedirs(dest_dir, exist_ok=True)
     dest_path = f'{dest_dir}/{src_name}'
+    txt_dest_path = f'{dest_dir}/{txt_src_name}'
     shutil.copy(src_path, dest_path)
+    shutil.copy(txt_src_path, txt_dest_path)
 
 def main():
     parser = argparse.ArgumentParser(usage = "Kill me")
     parser.add_argument("--input", "-i",  type = str, default = "bin/state.json", help = "Json path")
-    parser.add_argument("output", type = str, default = "output", help = "Output directory")
+    parser.add_argument("--output", "-o", type = str, default = "double_checked", help = "Output directory")
     args = parser.parse_args()
 
     output_dir = args.output
@@ -39,26 +43,26 @@ def main():
         temp = json_data['id'][sv]
 
         first_10 = list(file for file in json_data['id'][sv] if json_data['id'][sv][file]['accepted'])[:10]
-        for file in first_10:
-            src_path = json_data['id'][sv][file]['path']
-            copy_file(sv, src_path, output_dir, 'accepted')
+        if first_10:
+            for file in first_10:
+                src_path = json_data['id'][sv][file]['path']
+                copy_file(sv, src_path, output_dir, 'accepted')
 
         for dir in sv_dict[sv]:
-            accepted_list = list(file for file in sv_dict[sv][dir] if sv_dict[sv][dir][file]['accepted'])
-            not_accepted_list = list(file for file in sv_dict[sv][dir] if not sv_dict[sv][dir][file]['accepted'])
-            try:
-                if accepted_list:
-                    choice = random.choice(accepted_list)
-                    src_path = sv_dict[sv][dir][choice]['path']
-                    copy_file(sv, src_path, output_dir, 'accepted')
-                if not_accepted_list:
-                    choice = random.choice(not_accepted_list)
-                    src_path = sv_dict[sv][dir][choice]['path']
-                    copy_file(sv, src_path, output_dir, 'not_accepted')
-            except Exception as e:
-                print(e)
-                continue
-        break
+            accepted_list = list(file for file in sv_dict[sv][dir] if (sv_dict[sv][dir][file]['accepted'] and sv_dict[sv][dir][file] not in first_10))
+            not_accepted_list = list(file for file in sv_dict[sv][dir] if (not sv_dict[sv][dir][file]['accepted'] and sv_dict[sv][dir][file] not in first_10))
+            # try:
+            if accepted_list:
+                choice = random.choice(accepted_list)
+                src_path = sv_dict[sv][dir][choice]['path']
+                copy_file(sv, src_path, output_dir, 'accepted')
+            if not_accepted_list:
+                choice = random.choice(not_accepted_list)
+                src_path = sv_dict[sv][dir][choice]['path']
+                copy_file(sv, src_path, output_dir, 'not_accepted')
+            # except Exception as e:
+            #     print(e)
+            #     continue
 
 if __name__ == '__main__':
     main()
